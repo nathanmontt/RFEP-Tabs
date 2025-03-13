fb3efd954c19d0a0f1657719b76d25605e63a772
nathanmontt <contato.mont@gmail.com>
2025-03-13 17:54:56 -0300

    Entendimento
    Ainda não entendi como funciona as tabs.
    Acabei pegando meu primeiro conceito e depois mudei porque não faria muito sentido.
    O problema é que deu certo e eu NÃO ENTENDI O PORQUE.
    Estou buscando a explicação

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,3 +4,49 @@
 Para cada conteúdo prensente em cada tab, eles precisam estar "dentros um dos outros".
 Ou seja, um position: absolute; e a cada click em cada tab, deactive o anterior e active o que foi clicado.
 */
+
+function showTab(index) {
+    let tabContents = document.getElementsByClassName('tab-content');
+
+    for(let i=0 ; i<tabContents.length ; i++) {
+        tabContents[i].classList.remove("active");
+    }
+    tabContents[index].classList.add("active");
+}
+
+// function showTab(index) {
+
+//     // Retrieve tab content elements
+//     let tabContents =
+//         document.getElementsByClassName("tab-content");
+
+//     // Loop through all tab content elements
+//     for (let i = 0; i < tabContents.length; i++) {
+    
+//         // Remove the "active" class from all tab content elements
+//         tabContents[i].classList.remove("active");
+//     }
+    
+//     // Add the "active" class to the tab 
+//     // content element at the specified index
+//     tabContents[index].classList.add("active");
+// }
+
+
+
+// function showAndHideTabs(index) {
+//         // Retrieve tab content elements
+//         let tabContents =
+//         document.getElementsByClassName("content");
+
+//     // Loop through all tab content elements
+//     for (let i = 0; i < tabContents.length; i++) {
+    
+//         // Remove the "active" class from all tab content elements
+//         tabContents[i].classList.remove("active");
+//     }
+    
+//     // Add the "active" class to the tab 
+//     // content element at the specified index
+//     tabContents[index].classList.add("active");
+// }
