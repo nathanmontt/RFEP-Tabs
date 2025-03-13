bc089d53a9229857e3eb69bc61090f22d48cde45
nathanmontt <contato.mont@gmail.com>
2025-03-13 10:01:01 -0300

    Adição das imagens para o projeto

---
+++ src/assets/js/main.js
@@ -0,0 +1,6 @@
+'use strict'
+
+/* Minha visão sobre esse projeto é voltada para POSIÇÃO.
+Para cada conteúdo prensente em cada tab, eles precisam estar "dentros um dos outros".
+Ou seja, um position: absolute; e a cada click em cada tab, deactive o anterior e active o que foi clicado.
+*/
